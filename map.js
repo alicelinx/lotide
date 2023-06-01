082343cab5cda1ca5f8f701f0ecb52a7db085328
Alice Lin <alicexxlin@gmail.com>
2023-06-01 23:46:07 +0000

    Implement map

---
+++ map.js
@@ -0,0 +1,37 @@
+// The map function will return a new array based on the results of the callback function.
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (!eqArrays(arr1, arr2)) return console.log(`❌❌❌Assertion Failed: [${arr1}] !== [${arr2}]`)
+  else {
+    console.log(`✅✅✅Assertion Passed: [${arr1}] === [${arr2}] `)
+  }
+}
+
+const map = function(arr, callback) {
+  const results = [];
+  for (let item of arr) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+console.log(assertArraysEqual(results1, ['g', 'c', 't', 'm']))
+console.log(assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']))
+console.log(assertArraysEqual(results1, ['g', 'c', 't', 'm', 1]))
