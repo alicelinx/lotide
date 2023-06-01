9a6cc11e0dd52d41c8dca550ea0b7674c73bbe1d
Alice Lin <alicexxlin@gmail.com>
2023-06-01 00:07:45 +0000

    Implement countOnly in Lotide

---
+++ countOnly.js
@@ -0,0 +1,52 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: [${actual}] === [${expected}] `);
+  } else {
+    console.log(`❌❌❌Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  let result = {};
+
+  // loop over the array 
+  for (let item of allItems) {
+
+    // check if item matches object keys with truthy value
+    if (itemsToCount[item]) {
+
+      // console.log(item, result[item])
+
+      if (result[item]) {
+        result[item] += 1;
+      } else {
+        result[item] = 1;
+      }
+      // console.log(result)
+    }
+  }
+  return result;
+}
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
